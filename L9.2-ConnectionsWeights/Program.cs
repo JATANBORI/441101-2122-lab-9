bec40a62d072d694e80d3f0c26fb07ef31e5deca
John Atanbori <j.atanbori@hull.ac.uk>
2021-11-13 21:23:45 +0000

    initial Update

---
+++ L9.2-ConnectionsWeights/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace L6._2_ConnectionsWeights
+{
+    class Program
+    {
+        // Enter your codes here
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Neural Network Connections and Weights.");
+
+            Console.Write("Enter number of neuron in layer 1: ");
+            int neurons1 = Console.ReadLine();
+            Console.Write("Enter number of neuron in layer 2: ");
+            int neurons2 = Console.ReadLine();
+
+            // Enter your codes here
+
+            
+
+        }
+    }
+}
